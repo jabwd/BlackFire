7180a7c8c0cd8d1df67d8d6dca031419ec2ede4f
Antwan van Houdt <jabwdr@gmail.com>
2011-11-02 23:15:27 +0100

    Added some types to the XFSession header file

--- BlackFire/XFSession.h
+++ BlackFire/XFSession.h
@@ -6,6 +6,35 @@
 //  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+
+// login failure reasons
+extern NSString *XFVersionTooOldReason;
+extern NSString *XFInvalidPasswordReason;
+extern NSString *XFNetworkErrorReason;
+
+// Disconnect reasons
+extern NSString *XFOtherSessionReason;
+extern NSString *XFServerHungUpReason;
+extern NSString *XFServerStoppedRespondingReason;
+extern NSString *XFNormalDisconnectReason;
+
+extern NSString *XFFriendDidChangeNotification;
+extern NSString *XFFriendChangeAttribute;
+
+typedef enum
+{
+	XFShowMyFriendsOption = 0,
+	XFShowMyGameServerDataOption,
+	XFShowOnMyProfileOption,
+	XFShowChatTimeStampsOption,
+	XFShowFriendsOfFriendsOption,
+	XFShowMyOfflineFriendsOption,
+	XFShowNicknamesOption,
+	XFShowVoiceChatServerOption,
+	XFShowWhenITypeOption
+} XFPreferences;
+
 typedef enum{
 	XFSessionStatusOffline		= 0,
 	XFSessionStatusConnecting	= 1,
@@ -18,6 +47,8 @@ typedef enum{
 	XFSessionStatus _status;
 }
 
-@property (setter = setStatus:) XFSessionStatus status;
+@property (readonly) XFSessionStatus status;
+
+- (void)setStatus:(XFSessionStatus)newStatus;
 
 @end
--- BlackFire/XFSession.m
+++ BlackFire/XFSession.m
@@ -8,6 +8,18 @@
 
 #import "XFSession.h"
 
+NSString *XFVersionTooOldReason		= @"Version too old";
+NSString *XFInvalidPasswordReason	= @"Wrong username/password";
+NSString *XFNetworkErrorReason		= @"No internet connection";
+
+NSString *XFOtherSessionReason				= @"You logged in on another computer";
+NSString *XFServerHungUpReason				= @"Xfire server hung up";
+NSString *XFServerStoppedRespondingReason	= @"The Connection timed out";
+NSString *XFNormalDisconnectReason			= @"Normal disconnect";
+
+NSString *XFFriendDidChangeNotification		= @"XFFriendDidChangeNotification";
+NSString *XFFriendChangeAttribute			= @"XFFriendChangeAttribute";
+
 @implementation XFSession
 
 @synthesize status = _status;
