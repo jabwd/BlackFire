9b1fe7700719d8ef791fe3e3ee5bc710f815323a
Antwan van Houdt <jabwdr@gmail.com>
2011-11-05 00:10:10 +0100

    Added the XFGameServer class

---
+++ BlackFire/XFGameServer.h
@@ -0,0 +1,16 @@
+//
+//  XFGameServer.h
+//  BlackFire
+//
+//  Created by Antwan van Houdt on 11/5/11.
+//  Copyright (c) 2011 Exurion. All rights reserved.
+//
+//	Don't expect this to be a magic class that hosts a game server
+//	this class is just another "data" type like XFFriend that stores
+//	the user's favorite game servers
+
+#import <Foundation/Foundation.h>
+
+@interface XFGameServer : NSObject
+
+@end
---
+++ BlackFire/XFGameServer.m
@@ -0,0 +1,13 @@
+//
+//  XFGameServer.m
+//  BlackFire
+//
+//  Created by Antwan van Houdt on 11/5/11.
+//  Copyright (c) 2011 Exurion. All rights reserved.
+//
+
+#import "XFGameServer.h"
+
+@implementation XFGameServer
+
+@end
