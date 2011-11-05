7f8461ca53287c2a8f8910ab750e591ecd0aefba
Antwan van Houdt <jabwdr@gmail.com>
2011-11-05 12:12:13 +0100

    Added the XFGroupChat class

---
+++ BlackFire/XFGroupChat.h
@@ -0,0 +1,13 @@
+//
+//  XFGroupChat.h
+//  BlackFire
+//
+//  Created by Antwan van Houdt on 11/5/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface XFGroupChat : NSObject
+
+@end
---
+++ BlackFire/XFGroupChat.m
@@ -0,0 +1,13 @@
+//
+//  XFGroupChat.m
+//  BlackFire
+//
+//  Created by Antwan van Houdt on 11/5/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "XFGroupChat.h"
+
+@implementation XFGroupChat
+
+@end
