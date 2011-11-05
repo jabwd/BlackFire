fefacf8eb908b38648a06d1215c02aaa6c284ccd
Antwan van Houdt <jabwdr@gmail.com>
2011-11-05 16:27:39 +0100

    Added the BFAccount class

---
+++ BlackFire/BFAccount.h
@@ -0,0 +1,20 @@
+//
+//  BFAccount.h
+//  BlackFire
+//
+//  Created by Antwan van Houdt on 11/5/11.
+//  Copyright (c) 2011 Antwan van Houdt. All rights reserved.
+//
+
+
+
+@interface BFAccount : NSObject
+{
+	NSString *_username;
+	NSString *_password;
+}
+
+@property (nonatomic, retain) NSString *username;
+@property (nonatomic, retain) NSString *password;
+
+@end
---
+++ BlackFire/BFAccount.m
@@ -0,0 +1,24 @@
+//
+//  BFAccount.m
+//  BlackFire
+//
+//  Created by Antwan van Houdt on 11/5/11.
+//  Copyright (c) 2011 Antwan van Houdt. All rights reserved.
+//
+
+#import "BFAccount.h"
+
+@implementation BFAccount
+
+@synthesize username = _username;
+@synthesize password = _password;
+
+- (id)initWithIndex:(unsigned int)idx
+{
+	if( (self = [super init]) )
+	{
+		
+	}
+}
+
+@end
