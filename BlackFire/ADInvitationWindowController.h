5371fd82545cf313754b196b3a2a8e4ab8061af8
Antwan van Houdt <jabwdr@gmail.com>
2011-12-14 20:56:39 +0100

    Added a sheet controller for friend invitations

---
+++ BlackFire/ADInvitationWindowController.h
@@ -0,0 +1,13 @@
+//
+//  ADInvitationWindowController.h
+//  BlackFire
+//
+//  Created by Antwan van Houdt on 12/14/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADInvitationWindowController : NSObject
+
+@end
---
+++ BlackFire/ADInvitationWindowController.m
@@ -0,0 +1,13 @@
+//
+//  ADInvitationWindowController.m
+//  BlackFire
+//
+//  Created by Antwan van Houdt on 12/14/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ADInvitationWindowController.h"
+
+@implementation ADInvitationWindowController
+
+@end
