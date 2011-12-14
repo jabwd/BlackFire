5af5062cb1bfe15a2208d9d878b7dd5c4b947891
Antwan van Houdt <jabwdr@gmail.com>
2011-12-14 21:20:42 +0100

    Added UserDefault strings

---
+++ BlackFire/BFDefaults.h
@@ -0,0 +1,13 @@
+//
+//  BFDefaults.h
+//  BlackFire
+//
+//  Created by Antwan van Houdt on 12/14/11.
+//  Copyright (c) 2011 Antwan van Houdt. All rights reserved.
+//
+
+extern NSString *BFShowOfflineClanFriends;
+extern NSString *BFShowClanGroups;
+extern NSString *BFShowCustomFriendGroups;
+extern NSString *BFShowFriendsOfFriendsGroup;
+extern NSString *BFShowOfflineFriendsGroup;
---
+++ BlackFire/BFDefaults.m
@@ -0,0 +1,15 @@
+//
+//  BFDefaults.m
+//  BlackFire
+//
+//  Created by Antwan van Houdt on 12/14/11.
+//  Copyright (c) 2011 Antwan van Houdt. All rights reserved.
+//
+
+#import "BFDefaults.h"
+
+NSString *BFShowOfflineClanFriends		= @"showOfflineClanFriends";
+NSString *BFShowClanGroups				= @"showClanGroups";
+NSString *BFShowCustomFriendGroups		= @"showCustomFriendGroups";
+NSString *BFShowFriendsOfFriendsGroup	= @"showFriendsOfFriendsGroup";
+NSString *BFShowOfflineFriendsGroup		= @"showOfflineFriendsGroup";
