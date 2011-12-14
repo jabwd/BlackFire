51b016dddc6a81cf629c24a1823417a743d27ee3
Antwan van Houdt <jabwdr@gmail.com>
2011-12-14 20:56:15 +0100

    Added a preference window controller

---
+++ BlackFire/BFPreferencesWindowControllerWindowController.h
@@ -0,0 +1,13 @@
+//
+//  BFPreferencesWindowControllerWindowController.h
+//  BlackFire
+//
+//  Created by Antwan van Houdt on 12/14/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BFPreferencesWindowControllerWindowController : NSWindowController
+
+@end
