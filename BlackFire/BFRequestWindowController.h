57566c01e5cd2316ed2a8b0ef98809207a9d1707
Antwan van Houdt <jabwdr@gmail.com>
2011-12-17 19:54:46 +0100

    Added the friend request window controller

---
+++ BlackFire/BFRequestWindowController.h
@@ -0,0 +1,13 @@
+//
+//  BFRequestWindowController.h
+//  BlackFire
+//
+//  Created by Antwan van Houdt on 12/17/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BFRequestWindowController : NSObject
+
+@end
