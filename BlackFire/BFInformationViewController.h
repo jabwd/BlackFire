049759517535d800105c8a60a16939f315437d2e
Antwan van Houdt <jabwdr@gmail.com>
2012-03-02 20:27:11 +0100

    Added the information view and controller

---
+++ BlackFire/BFInformationViewController.h
@@ -0,0 +1,13 @@
+//
+//  BFInformationViewController.h
+//  BlackFire
+//
+//  Created by Antwan van Houdt on 3/2/12.
+//  Copyright (c) 2012 Exurion. All rights reserved.
+//
+
+#import "BFTabViewController.h"
+
+@interface BFInformationViewController : BFTabViewController
+
+@end
---
+++ BlackFire/BFInformationViewController.m
@@ -0,0 +1,28 @@
+//
+//  BFInformationViewController.m
+//  BlackFire
+//
+//  Created by Antwan van Houdt on 3/2/12.
+//  Copyright (c) 2012 Exurion. All rights reserved.
+//
+
+#import "BFInformationViewController.h"
+
+@implementation BFInformationViewController
+
+- (id)init
+{
+	if( (self = [super init]) )
+	{
+		[NSBundle loadNibNamed:@"InformationView" owner:self];
+	}
+}
+
+- (void)dealloc
+{
+	[super dealloc];
+}
+
+#pragma mark - View controlling
+
+@end
